ebb9a90b6b13273f57d7736bb7bab7f10716b978
wpinillo-un <wpinillo@unal.edu.co>
2025-05-24 08:29:19 -0500

    Factorización del código

---
+++ homework/src/_internals/calculate_metrics.py
@@ -0,0 +1,12 @@
+# Metricas de error durante entrenamiento
+
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+
+def calculate_metrics(model, x, y):
+    y_pred = model.predict(x)
+    mse = mean_squared_error(y, y_pred)
+    mae = mean_absolute_error(y, y_pred)
+    r2 = r2_score(y, y_pred)
+
+    return mse, mae, r2
